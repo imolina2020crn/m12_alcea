8901fb374a9ce982130107128547cf7c7750a50f
imolina2020crn <imolina.crn@artsgrafiques.org>
2021-05-12 19:47:59 +0200

    123123

---
+++ src_byisma/js/notifications.js
@@ -0,0 +1,14 @@
+function openNav() {
+	document.getElementById("mySidenav").style.width = "16rem";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.body.style.backgroundColor = "white";
+}
+
+$(document).ready(function(){
+	$("#notificationsItem").addClass('sidenavSelected');
+});
+    
