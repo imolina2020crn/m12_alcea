e3087f3ac5c0288c3d0d150cdb80c0e4d2018bb5
imolina2020crn <imolina.crn@artsgrafiques.org>
2021-05-16 16:15:06 +0200

    123123

---
+++ alcea_app_final/src/js/profile.js
@@ -0,0 +1,21 @@
+function openNav() {
+	document.getElementById("mySidenav").style.width = "16rem";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+	document.getElementById("mainContainer").style.opacity = "0.4";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.body.style.backgroundColor = "white";
+
+	document.getElementById("mainContainer").style.opacity = "1";
+}
+
+function showSettings(){
+	window.location.href = '../pages/settings.html';
+}
+
+$(document).ready(function(){
+	$("#profileItem").addClass('sidenavSelected');
+});
